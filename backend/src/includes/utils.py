c24c076a4aea0d78e5f3f3f18b3f8acce2068cb7
Robert Alexander <gogonegro@gmail.com>
2025-01-15 08:56:56 +0100

    fixing docker and connect to custom weaviate client to container service name

--- backend/src/includes/utils.py
+++ backend/src/includes/utils.py
@@ -31,11 +31,16 @@ def init_weaviate_client() -> weaviate.WeaviateClient:
         if os.getenv("COP_WEAVIATE_URL") == "localhost":
             client = weaviate.connect_to_local()
         else:
-            client = weaviate.connect(
-                connection_params=weaviate.auth.AuthApiKey(
+            client = weaviate.connect_to_custom(
+                http_host=os.getenv("COP_WEAVIATE_URL"),
+                http_port=8080,
+                http_secure=False,
+                grpc_host=os.getenv("COP_WEAVIATE_URL"),
+                grpc_port=50051,
+                grpc_secure=False,
+                auth_credentials=weaviate.auth.AuthApiKey(
                     api_key=os.getenv("WEAVIATE_API_KEY"),
                 ),
-                host=os.getenv("COP_WEAVIATE_URL"),
             )
     except Exception as e:
         error_message = "Failed to initialize Weaviate client"
